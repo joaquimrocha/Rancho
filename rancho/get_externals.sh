ea62c388773e579925c300866506487982f92937
Luis Rodrigues <lfrodrigues@gmail.com>
2009-06-25 00:30:35 +0100

    added gitignore and script to get external svn repositories

---
+++ rancho/get_externals.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+svn co -r 46 http://django-pagination.googlecode.com/svn/trunk/pagination pagination
+svn co -r 154 http://django-tagging.googlecode.com/svn/trunk/tagging/ tagging
+svn co -r 49 http://django-mailer.googlecode.com/svn/trunk/mailer/ mailer
+svn co -r 60 http://django-tinymce.googlecode.com/svn/trunk/tinymce/ tinymce
+svn co -r 35 http://django-timezones.googlecode.com/svn/trunk/timezones/ timezones
+svn co -r 125 http://django-notification.googlecode.com/svn/trunk/notification notification
+
+
+
