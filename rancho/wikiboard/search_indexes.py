6caf2a8f7807bcc99096a1a05c0df4b76cd00918
Luis Rodrigues <lfrodrigues@gmail.com>
2009-11-04 18:53:32 +0000

    fixed typo in index

--- rancho/wikiboard/search_indexes.py
+++ rancho/wikiboard/search_indexes.py
@@ -4,5 +4,5 @@ from rancho.wikiboard.models import WikiEntry
 class WikiEntryIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     project = indexes.IntegerField(model_attr='project', use_template=True)
-    user = indexes.CharField(model_attr='creator')
+    user = indexes.CharField(model_attr='author')
 site.register(WikiEntry, WikiEntryIndex)
