528b66808d5307a062bd3a39e7a39155f672a53a
Luis Rodrigues <lfrodrigues@gmail.com>
2009-11-21 02:46:44 +0000

    updated externals and updated cron code

--- rancho/get_externals.sh
+++ rancho/get_externals.sh
@@ -2,13 +2,19 @@
 
 svn co -r 46 http://django-pagination.googlecode.com/svn/trunk/pagination pagination
 svn co -r 154 http://django-tagging.googlecode.com/svn/trunk/tagging/ tagging
-svn co -r 49 http://django-mailer.googlecode.com/svn/trunk/mailer/ mailer
 svn co -r 60 http://django-tinymce.googlecode.com/svn/trunk/tinymce/ tinymce
 svn co -r 35 http://django-timezones.googlecode.com/svn/trunk/timezones/ timezones
-svn co -r 125 http://django-notification.googlecode.com/svn/trunk/notification notification
-svn co -r 348 http://svn.whoosh.ca/projects/whoosh/trunk/src/whoosh/ whoosh
+svn co -r 353 http://svn.whoosh.ca/projects/whoosh/trunk/src/whoosh/ whoosh
 
 #get django-haystack
 git clone git://github.com/toastdriven/django-haystack.git 
 mv django-haystack/haystack haystack
 rm -rf django-haystack
+
+git clone git://github.com/jtauber/django-notification.git
+mv django-notification/notification/ .
+rm -rf django-notification/
+
+git clone git://github.com/jtauber/django-mailer.git 
+mv django-mailer/mailer/ .
+rm -rf django-mailer/
--- rancho/lib/management/commands/run_cron.py
+++ rancho/lib/management/commands/run_cron.py
@@ -1,12 +1,21 @@
-import logging
 from django.core.management.base import NoArgsCommand
-from mailer.engine import send_all
-from milestone.cron import run_milestone_cron
+from rancho.milestone.cron import run_milestone_cron
+import logging
 
 class Command(NoArgsCommand):
     help = 'Run the rancho Cron'
     
     def handle_noargs(self, **options):
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-        print "Milestone's cron..."
-        run_milestone_cron()  
+        try:
+            run_milestone_cron()
+        except Exception, inst:
+            print "milestone problem", inst
+              
+        try:
+            from rancho.mailer.management.commands.retry_deferred import Command as C_retry_deferred
+            from rancho.mailer.management.commands.send_mail import Command as C_send_mail        
+            C_retry_deferred().handle_noargs()
+            C_send_mail().handle_noargs() 
+        except Exception, inst:
+            print "send mail problems", inst
