bf100777142378011fa5c3c77cfb5cee119b16e6
Luis Rodrigues <lfrodrigues@gmail.com>
2009-12-08 17:47:58 +0000

    fixed file len sizes

---
+++ rancho/file/migrations/0002_increase_file_len.py
@@ -0,0 +1,113 @@
+
+from south.db import db
+from django.db import models
+from rancho.file.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Changing field 'File.title'
+        # (to signature: django.db.models.fields.CharField(max_length=500))
+        db.alter_column('file_file', 'title', orm['file.file:title'])
+        
+        # Changing field 'FileVersion.file_location'
+        # (to signature: django.db.models.fields.files.FileField(max_length=500))
+        db.alter_column('file_fileversion', 'file_location', orm['file.fileversion:file_location'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Changing field 'File.title'
+        # (to signature: django.db.models.fields.CharField(max_length=50))
+        db.alter_column('file_file', 'title', orm['file.file:title'])
+        
+        # Changing field 'FileVersion.file_location'
+        # (to signature: django.db.models.fields.files.FileField(max_length=100))
+        db.alter_column('file_fileversion', 'file_location', orm['file.fileversion:file_location'])
+        
+    
+    
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'company.company': {
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True'}),
+            'display_logo_name': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'long_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'mailing_address': ('django.db.models.fields.TextField', [], {'max_length': '500', 'null': 'True'}),
+            'main_company': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'webpage': ('django.db.models.fields.URLField', [], {'max_length': '500', 'null': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'file.file': {
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_file_version': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'lastversion'", 'null': 'True', 'to': "orm['file.FileVersion']"}),
+            'notify_to': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'null': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.Project']"}),
+            'tags': ('TagField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
+        },
+        'file.fileversion': {
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'uploader'", 'to': "orm['auth.User']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True'}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['file.File']"}),
+            'file_location': ('django.db.models.fields.files.FileField', [], {'max_length': '500'}),
+            'file_size': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'file_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'project.project': {
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['company.Company']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'creator'", 'to': "orm['auth.User']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True'}),
+            'finish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']"})
+        }
+    }
+    
+    complete_apps = ['file']
