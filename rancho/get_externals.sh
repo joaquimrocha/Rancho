78c78c346871f4e8024ea085555d4af77371e962
Joaquim Rocha <joaquimrocha1@gmail.com>
2012-01-10 21:57:51 +0100

    Updated the dependencies URLs in get_externals.sh
    Also have changed it so it uses a a script for repeated commands

--- rancho/get_externals.sh
+++ rancho/get_externals.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-svn co -r 46 http://django-pagination.googlecode.com/svn/trunk/pagination pagination
-svn co -r 154 http://django-tagging.googlecode.com/svn/trunk/tagging/ tagging
-svn co -r 60 http://django-tinymce.googlecode.com/svn/trunk/tinymce/ tinymce
-svn co -r 35 http://django-timezones.googlecode.com/svn/trunk/timezones/ timezones
-svn co -r 353 http://svn.whoosh.ca/projects/whoosh/trunk/src/whoosh/ whoosh
+URLS="git://github.com/jtauber/django-notification.git
+      git://github.com/jtauber/django-mailer.git
+      https://github.com/ericflo/django-pagination.git
+      https://github.com/brosner/django-tagging.git
+      https://github.com/aljosa/django-tinymce.git
+      https://github.com/brosner/django-timezones.git"
 
-#get django-haystack
-git clone git://github.com/toastdriven/django-haystack.git 
-mv django-haystack/haystack haystack
+for giturl in $URLS; do
+    projectname=`echo $giturl | sed 's/.*\(django\-.*\)\.git/\1/'`
+    modulename=`echo $projectname | sed 's/.*django\-\(.*\)$/\1/'`
+    git clone $giturl
+    mv $projectname/$modulename .
+    rm -rf $projectname
+done
+
+git clone git://github.com/toastdriven/django-haystack.git
+cd django-haystack && git checkout v1.2.6; cd ..
+mv django-haystack/haystack .
 rm -rf django-haystack
 
-git clone git://github.com/jtauber/django-notification.git
-mv django-notification/notification/ .
-rm -rf django-notification/
-
-git clone git://github.com/jtauber/django-mailer.git 
-mv django-mailer/mailer/ .
-rm -rf django-mailer/
+hg clone https://bitbucket.org/mchaput/whoosh whoosh.repo
+mv whoosh.repo/src/whoosh .
+rm -rf whoosh.repo
